9cfd45e3d4d1666b30d0a1dece21b3b140b226cf
Bj190 <bryn.jones019@gmail.com>
2023-11-14 19:51:17 +0000

    test

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,6 +13,12 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
+  @Override
+  public boolean add( Product pr )
+  {                              
+    return super.add( pr );     // Call add in ArrayList
+  }
+
 
   // You need to add code here
 }
