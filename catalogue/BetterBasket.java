8084501bfb7b09b2b3283b784289f33987f2ce32
Bj190 <bryn.jones019@gmail.com>
2023-11-16 12:31:58 +0000

    Bettercashier

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -1,7 +1,9 @@
 package catalogue;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Write a description of class BetterBasket here.
@@ -12,13 +14,33 @@ import java.util.Collections;
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
+ 
+  
+  public String productList(Product pr) {
+	  double total2 = 0.0;
+	  ArrayList<String> products = new ArrayList<String>();
+	  for(int i = 0; i < products.size(); i++) {
+		  if(pr.equals(products.get(i))){
+			  int number2 = pr.getQuantity();
+			  number2++;
+			  total2 = pr.getPrice() * number2;
+		  }
+		  else {
+			  super.add(pr);
+		  }
+	  }
+	  //to be sorted
+	  return productList(products);
+  }
   
   @Override
-  public boolean add( Product pr )
-  {                              
-      // Call add in ArrayList
+  public boolean add ( Product pr)
+  {       
+	super.add(pr); 
+	productList(pr);   
+	  
   }
-
+ 
 
   // You need to add code here
 }
